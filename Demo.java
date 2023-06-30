4259045630979b6b71099fddf8cbc9cde13cc645
Ha Ngoc Linh <linhhn13@fe.edu.vn>
2023-06-30 20:26:51 +0700

    add demo

---
+++ Demo.java
@@ -0,0 +1,111 @@
+
+import java.security.MessageDigest;
+
+class HashingWorker {
+    public static String sha256(final String base) {
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
+
+class MyThread implements Runnable {
+    /**
+     * Luồng
+     */
+    private Thread t;
+
+    /**
+     * Tên luồng
+     */
+    String name;
+
+    Integer maxNum;
+
+    Flag flag;
+
+    /**
+     * constructor
+     */
+    MyThread(String _name, Flag _flag, Integer _maxNum) {
+        this.name = _name;
+        System.out.println("running MyThread constructor " + name);
+        this.flag = _flag;
+        this.maxNum = _maxNum;
+    }
+
+    /**
+     * implement interface của Runnable
+     */
+    public void run() {
+        for(int i = 0; i < maxNum ; i++){
+            // check cờ
+            if (flag.isStop) {
+                break;
+            }
+            System.out.println("Thread " + name + " : "+ i);
+        }
+        //vẫy cờ
+        flag.setDone();
+    }
+
+    /**
+     * Hàm start thread
+     */
+    public void start() {
+        System.out.println("running MyThread " + name + "is starting");
+        // kiểm tra xem thread đã được khởi tạo hay chưa nếu chưa thì khởi tạo
+        if (t == null) {
+            // truyền vào Runnable và tên của thread (tên thread không được trùng nhau)
+            t = new Thread(this, name);
+            t.start();
+        }
+    }
+}
+
+public class Demo {
+    Flag flag;
+    public static void main(String[] args) {
+       long startTime = System.nanoTime();
+
+       Flag flag = new Flag();
+        
+        // khai báo instance của class MyThread
+        MyThread mt1 = new MyThread("Thread1", flag, 10);
+        mt1.start();
+
+        // khai báo instance của class MyThread
+        MyThread mt2 = new MyThread("Thread2", flag, 100000);
+        mt2.start();
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println(duration);
+    }
+}
+
+/**
+ * Cờ đánh dấu nhiệm vụ đã xong
+ */
+class Flag {
+    Boolean isStop = false;
+    
+    public void setDone(){
+        isStop = true;
+    }
+
+    public Boolean isDone(){
+        return isStop;
+    }
+}
--- Timming.java
+++ Timming.java
@@ -1,6 +1,7 @@
 public class Timming {
     public static void main() {
         long startTime = System.nanoTime();
+        // your code in here
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
         System.out.println(duration);
